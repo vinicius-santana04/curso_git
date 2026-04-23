28fa1bd9f8421ac47bb7bcb49be12ac53296b43f
vinicius-santana04 <viniciusantangato@gmail.com>
2026-04-22 22:51:32 -0400

    primeiro commit

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from datetime import datetime
 
 def saudacao_atleta(nome: str, equipe: str, posicao_tática: str) -> dict:
-    """
-    Retorna uma mensagem de boas-vindas e um payload de dados do atleta.
-    """
+    
     mensagem = f"Olá, {nome}! Bem-vindo ao sistema de análise da equipe {equipe}."
     
     payload = {
@@ -16,7 +14,6 @@ def saudacao_atleta(nome: str, equipe: str, posicao_tática: str) -> dict:
     
     return payload
 
-# Testando a função
 dados_jogador = saudacao_atleta(nome="Camisa 10", equipe="Time Principal", posicao_tática="Meio-Campo")
 
 print(dados_jogador["mensagem_boas_vindas"])
